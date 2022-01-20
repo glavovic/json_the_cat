ebfabcaa25d1ba0dd39751ee48ff8662d6a839d8
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-20 23:31:06 +0000

    added test and refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require('request');
-const argv = process.argv.slice(2);
 
+const fetchBreedDescription = function(breedName, callback) {
 
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      return callback('Error was found in Website', null);
+    } else {
+      const data = JSON.parse(body);
+      if (data[0]) {
+        return callback(null, data[0].description);
+      } else {
+        return callback(console.log('Breed not found'), null);
+      }
+    }
+  });
+};
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${argv[0]}`, function(error, response, body) {
-  if (error) {
-    console.log('error', error);
-    return;
-  }
-  const data = JSON.parse(body);
-
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed not found');
-  }
-  
-});
-
+module.exports = { fetchBreedDescription };
